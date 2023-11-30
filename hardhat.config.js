f778cd47667e1fac0edda05a62b19c73062e296d
tewodroslm <tewodroslemma64@gmail.com>
2023-11-30 07:17:28 -0500

    deploy to sepolia test net sucess

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
+  // hardhat comes with internal network by default , that is why it is easyier to do local dev
+  defaultNetwork: "hardhat",
+  // here i will give my rpc url from alchemy .. to deploy to sepolia faucet testnet
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      // chainId: 58008,
+    },
+  },
   solidity: "0.8.19",
 };
