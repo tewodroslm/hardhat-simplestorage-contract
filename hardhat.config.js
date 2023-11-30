f660a88f9f5250bc2b04a081866539dcd5d99f1f
tewodroslm <tewodroslemma64@gmail.com>
2023-11-30 08:56:03 -0500

    etherscan api key for verfication

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+// require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 module.exports = {
   // hardhat comes with internal network by default , that is why it is easyier to do local dev
   defaultNetwork: "hardhat",
@@ -13,8 +15,14 @@ module.exports = {
     sepolia: {
       url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
-      // chainId: 58008,
+      chainId: 11155111,
     },
   },
   solidity: "0.8.19",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
