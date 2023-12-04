25fac7cdbda3a3b22df27a3338cc55931cb6b106
tewodroslm <tewodroslemma64@gmail.com>
2023-12-04 08:32:44 -0500

    verify programmaticaly, deploy, store and retrieve

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,62 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+// import
 const hre = require("hardhat");
+const run = require("hardhat");
+const network = require("hardhat");
+// const { hre, run, network } = require("hardhat").hardhatArguments;
 
+// async main
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  console.log("start main..");
+  const simpleStorage = await hre.ethers.deployContract("SimpleStorage", []);
+  console.log("deploy call ... awaiting ..");
+  await simpleStorage.waitForDeployment();
+  console.log(`SimpleStorage address: ${simpleStorage.target}`);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  // when verifiying check which  network , if remote and not local call verify function
+  // 11155111 is sepolia
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    await verify(simpleStorage.target, []);
+  }
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current value is: ${currentValue}`);
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // update currentvalue
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated value is: ${updatedValue}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// verfiy and publish programmaticlly instead of doing it manually on the test net
+// contract address is needed at least.. arg is the value that is being sent to the constructor of our sol file
+// auto verfication could be done on ethscan docs.etherscan.io .. looking at the api they provide and consuming it
+// or use already existing hardhat etherscan plugin like the hardhat-etherscan plugin from npmjs.com
+// async function verify(contractAddress, arg) {}
+// using "run" to run any hardhat task after import we can programmatically verify or
+// use ' npx hardhat verify --network sepolia <address> ' command to verify with out a code
+// we don't want to call verfiy fn when working local  network and that is where chainId comes to be usefull
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verfied")) {
+      console.log("Already Verfied");
+    } else {
+      console.log(e);
+    }
+  }
+}
+
+// call main
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
