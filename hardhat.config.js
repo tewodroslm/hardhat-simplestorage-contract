718b5b3d1d5615f290d7feddc6d45ce49b9bb84f
tewodroslm <tewodroslemma64@gmail.com>
2024-04-04 13:16:46 -0400

    comment added for clarity

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,10 +9,11 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 module.exports = {
   // hardhat comes with internal network by default , that is why it is easyier to do local dev
-  defaultNetwork: "hardhat",
+  defaultNetwork: "hardhat", // local hardhat network will be used when "npx hardhat run scripts/deploy.js"
   // here i will give my rpc url from alchemy .. to deploy to sepolia faucet testnet
   networks: {
     sepolia: {
+      // deploying to remote sepolia network when "npx hardhat run scripts/deploy.js --network sepolia"
       url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 11155111,
@@ -25,7 +26,7 @@ module.exports = {
   },
   solidity: "0.8.19",
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+    apiKey: ETHERSCAN_API_KEY, // used to verify my contract , key is from https://etherscan.io/myapikey
   },
   sourcify: {
     enabled: true,
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,23 +1,26 @@
 // import
 const hre = require("hardhat");
-const run = require("hardhat");
-const network = require("hardhat");
+// const run = require("hardhat");
+// const network = require("hardhat");
 // const { hre, run, network } = require("hardhat").hardhatArguments;
 
 // async main
 async function main() {
   console.log("start main..");
+
+  // deploy our contract
   const simpleStorage = await hre.ethers.deployContract("SimpleStorage", []);
   console.log("deploy call ... awaiting ..");
   await simpleStorage.waitForDeployment();
   console.log(`SimpleStorage address: ${simpleStorage.target}`);
 
-  // when verifiying check which  network , if remote and not local call verify function
+  // when verifiying check which  network , for only remote net and do not verify local hardhat network
   // 11155111 is sepolia
-  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+  if (hre.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
     await verify(simpleStorage.target, []);
   }
 
+  // interact with our contract
   const currentValue = await simpleStorage.retrieve();
   console.log(`Current value is: ${currentValue}`);
 
@@ -28,13 +31,15 @@ async function main() {
   console.log(`Updated value is: ${updatedValue}`);
 }
 
-// verfiy and publish programmaticlly instead of doing it manually on the test net
+// verfiying and publish programmaticlly instead of doing it manually on the test net
 // contract address is needed at least.. arg is the value that is being sent to the constructor of our sol file
 // auto verfication could be done on ethscan docs.etherscan.io .. looking at the api they provide and consuming it
 // or use already existing hardhat etherscan plugin like the hardhat-etherscan plugin from npmjs.com
 // async function verify(contractAddress, arg) {}
-// using "run" to run any hardhat task after import we can programmatically verify or
-// use ' npx hardhat verify --network sepolia <address> ' command to verify with out a code
+
+// BUT simple way also using "run" to run any hardhat VERIFY task
+// use ' npx hardhat verify --network sepolia <address> ' command to verify WITH OUT WRTING THE BELOW VERIFY() CODE
+
 // we don't want to call verfiy fn when working local  network and that is where chainId comes to be usefull
 
 async function verify(contractAddress, args) {
