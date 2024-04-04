41ce99b8ed17e19fcc715889b8ab1deb76ba2690
tewodroslm <tewodroslemma64@gmail.com>
2024-04-04 15:29:02 -0400

    test update and cleaned

--- test/test-deploy.js
+++ test/test-deploy.js
@@ -1,16 +1,25 @@
-// const { ethers } = require("hardhat");
+const { ethers } = require("hardhat");
+const { assert } = require("chai");
 
-// describe("simple storage", () => {
-//   let simpleStorage;
-//   let simpleStorageFactory;
-//   beforeEach(async function () {
-//     simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
-//     simpleStorage = await simpleStorageFactory.deploy();
-//   });
+describe("simple storage", () => {
+  let simpleStorage;
+  let simpleStorageFactory;
+  beforeEach(async function () {
+    simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+    simpleStorage = await simpleStorageFactory.deploy();
+  });
 
-//   it("should start favorite number with 0", async function () {
-//     const currentValue = await simpleStorage.retreive();
-//     const expectedValue = "0";
-//     assert.equals(currentValue.toString(), expectedValue);
-//   });
-// });
+  it("should start favorite number with 0", async function () {
+    const currentValue = await simpleStorage.retrieve();
+    const expectedValue = "0";
+    assert.equal(currentValue.toString(), expectedValue);
+  });
+
+  it("should update favorite number", async function () {
+    const expectedValue = "7";
+    const transactionResponse = await simpleStorage.store("7");
+    await transactionResponse.wait(1);
+    const currentValue = await simpleStorage.retrieve();
+    assert.equal(currentValue.toString(), expectedValue);
+  });
+});
